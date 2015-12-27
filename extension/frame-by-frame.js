ee227101f17a8f44bb6377e66d4543e8bfcdec00
regularWand <rgwnd@gmx.com>
2015-12-26 19:17:01 -0500

    Moved code to new function loaded via window.onload()
    Ensures the page is fully loaded before running the script, avoiding
    grabbing of elements before they are defined.

--- extension/frame-by-frame.js
+++ extension/frame-by-frame.js
@@ -1,181 +1,192 @@
-var fbf = {};
+frameByFrame = function() {
+	var fbf = {};
 
-fbf.FRAMES_PER_SECOND = 25;
-fbf.PLAYER_ID = "movie_player";
-fbf.LEFT_SQUARE_BRACKET = 219;
-fbf.RIGHT_SQUARE_BRACKET = 221;
-fbf.COMMA = 188;
-fbf.PERIOD = 190;
-fbf.P_KEY = 80;
-fbf.O_KEY = 79;
-fbf.BACKSLASH_KEY = 220;
-var frameskip = 1;
-var hotkeys = true;
-var player = document.getElementById(fbf.PLAYER_ID);
-var pbar = document.getElementsByClassName("ytp-progress-bar")[0];
-var control_bar = document.getElementsByClassName("ytp-chrome-controls")[0];
-var shadow = document.getElementsByClassName("ytp-gradient-bottom")[0];
-var header = document.getElementById("watch-header");
-var controlsToggle = false;
+	fbf.FRAMES_PER_SECOND = 25;
+	fbf.PLAYER_ID = "movie_player";
+	fbf.LEFT_SQUARE_BRACKET = 219;
+	fbf.RIGHT_SQUARE_BRACKET = 221;
+	fbf.COMMA = 188;
+	fbf.PERIOD = 190;
+	fbf.P_KEY = 80;
+	fbf.O_KEY = 79;
+	fbf.BACKSLASH_KEY = 220;
+	var frameskip = 1;
+	var hotkeys = true;
+	var player = document.getElementById(fbf.PLAYER_ID);
+	var pbar = document.getElementsByClassName("ytp-progress-bar")[0];
+	var control_bar = document.getElementsByClassName("ytp-chrome-controls")[0];
+	var controlsToggle = false;
+	var shadow = document.getElementsByClassName("ytp-gradient-bottom")[0];
+	var header = document.getElementById("watch-header");
+	var brand = document.getElementsByClassName("iv-branding")[0];
 
-fbf.prevFrame = function(frameskip) {
-    // Based on YouTube enhancer userscript, http://userscripts.org/scripts/show/33042.
-    player.pauseVideo();
-    player.seekBy(-frameskip * (1/fbf.FRAMES_PER_SECOND));
-}
-
-fbf.nextFrame = function(frameskip) {
-    // Based on YouTube enhancer userscript, http://userscripts.org/scripts/show/33042.
-    player.pauseVideo();
-    player.seekBy(frameskip * (1/fbf.FRAMES_PER_SECOND));
-}
-
-fbf.fbfPlayback = function() {
-	if (player.getPlaybackRate()===0.25) {
-		player.setPlaybackRate(1);
+	fbf.prevFrame = function(frameskip) {
+		// Based on YouTube enhancer userscript, http://userscripts.org/scripts/show/33042.
+		player.pauseVideo();
+		player.seekBy(-frameskip * (1/fbf.FRAMES_PER_SECOND));
 	}
-	else {
-		player.setPlaybackRate(0.25);
-	}
-}
 
-fbf.setFrameRate = function() {
-	if (fbf.FRAMES_PER_SECOND===25) {
-		fbf.FRAMES_PER_SECOND = 30;
+	fbf.nextFrame = function(frameskip) {
+		// Based on YouTube enhancer userscript, http://userscripts.org/scripts/show/33042.
+		player.pauseVideo();
+		player.seekBy(frameskip * (1/fbf.FRAMES_PER_SECOND));
 	}
-	else {
-		fbf.FRAMES_PER_SECOND = 25;
+
+	fbf.fbfPlayback = function() {
+		if (player.getPlaybackRate()===0.25) {
+			player.setPlaybackRate(1);
+		}
+		else {
+			player.setPlaybackRate(0.25);
+		}
 	}
-	fbf.updateFpsAndFS();
-}
 
-fbf.multiplyFS = function(factor) {
-	if (factor==="decrease" && frameskip >=2){
-		frameskip/=2;
-	} 
-	if (factor==="increase" && frameskip <=16) {
-		frameskip*=2;
+	fbf.setFrameRate = function() {
+		if (fbf.FRAMES_PER_SECOND===25) {
+			fbf.FRAMES_PER_SECOND = 30;
+		}
+		else {
+			fbf.FRAMES_PER_SECOND = 25;
+		}
+		fbf.updateFpsAndFS();
 	}
-	fbf.updateFpsAndFS();
-}
 
-fbf.hideControls= function() {
-	if (!controlsToggle) {
-	pbar.style.visibility="hidden";
-	control_bar.style.visibility="hidden";
-	shadow.style.visibility="hidden";
-	controlsToggle = true;
-	} else {
-	pbar.style.visibility="visible";
-	control_bar.style.visibility="visible";
-	shadow.style.visibility="visible";
-	controlsToggle = false;
+	fbf.multiplyFS = function(factor) {
+		if (factor==="decrease" && frameskip >=2){
+			frameskip/=2;
+		} 
+		if (factor==="increase" && frameskip <=16) {
+			frameskip*=2;
+		}
+		fbf.updateFpsAndFS();
 	}
-}
 
-fbf.injectControls = function() {
-    var controls_html = "<i class=\"icon icon-to-start\"></i><i class=\"icon icon-to-end\"></i>";
-    var fpsAndframeskip_html = "<b>FPS:&nbsp;" + fbf.FRAMES_PER_SECOND + "</b>\
-	&nbsp;&nbsp;<b>Frameskip:&nbsp;" + frameskip + "</b>";
-	var hotkeysButton_html = "<i class=\"icon icon-for-hotkeys-menu\"></i>";
-		
-    var newButtons = document.createElement('div');
-    newButtons.innerHTML = controls_html;
-    newButtons.style.float = 'left';
-    newButtons.style['margin-top'] = '2px';
-	
-	var hotkeysButton = document.createElement('div');
-    hotkeysButton.innerHTML = hotkeysButton_html;
-    hotkeysButton.style.float = 'right';
-	hotkeysButton.style['margin-top'] = '2px';
-	
-	var fpsAndframeskip = document.createElement('div');
-    fpsAndframeskip.innerHTML = fpsAndframeskip_html;
-    fpsAndframeskip.style.float = 'right';
-	fpsAndframeskip.style['margin-top'] = '2px';
-	
-    var child = document.getElementsByClassName('ytp-volume-hover-area')[0];
-	
-    control_bar.insertBefore(newButtons, child);
-	control_bar.insertBefore(hotkeysButton, child);
-	control_bar.insertBefore(fpsAndframeskip, child);
-	
-	fbf.updateFpsAndFS = function() {
-		fpsAndframeskip_html = "<b>FPS:&nbsp;" + fbf.FRAMES_PER_SECOND + "</b>\
-		&nbsp;&nbsp;<b>Frameskip:&nbsp;" + frameskip + "</b>";
-		fpsAndframeskip.innerHTML = fpsAndframeskip_html;
-	}
-	
-    var forward_button = document.getElementsByClassName("icon-to-end")[0];
-    forward_button.addEventListener('click', function() {
-        fbf.nextFrame(frameskip);
-    });
-
-    var back_button = document.getElementsByClassName("icon-to-start")[0];
-    back_button.addEventListener('click', function() {
-        fbf.prevFrame(frameskip);
-    });
-	
-	hotkeysButton.addEventListener('click', function(){
-		newwindow=window.open("http://codepen.io/regularWand/full/eJdVep\
-		","name","height=510, width=467, top=125");
-		if (window.focus) {newwindow.focus()}
-	});
-}
-
-if (document.getElementsByClassName("ytp-chrome-controls")[0]) {
-    fbf.injectControls();
-	
-	document.addEventListener("keydown", function(e) {
-	if (hotkeys) {
-			switch(e.which) {
-				case fbf.LEFT_SQUARE_BRACKET:
-					fbf.prevFrame(frameskip);
-					break;
-				case fbf.RIGHT_SQUARE_BRACKET:
-					fbf.nextFrame(frameskip);
-					break;
-				case fbf.COMMA:
-					fbf.multiplyFS("decrease");
-					break;
-				case fbf.PERIOD:
-					fbf.multiplyFS("increase");
-					break;
-				case fbf.P_KEY:
-					fbf.fbfPlayback();
-					break;
-				case fbf.O_KEY:
-					fbf.setFrameRate();
-					break;
-				case fbf.BACKSLASH_KEY:
-					fbf.hideControls();
-					break;
+	fbf.hideControls= function() {
+		if (!controlsToggle) {
+		pbar.style.visibility="hidden";
+		control_bar.style.visibility="hidden";
+		shadow.style.visibility="hidden";
+			if (brand) {
+				brand.style.visibility="hidden";
 			}
+		controlsToggle = true;
+		} else {
+		pbar.style.visibility="visible";
+		control_bar.style.visibility="visible";
+		shadow.style.visibility="visible";
+			if (brand) {
+				brand.style.visibility="visible";
+			}
+		controlsToggle = false;
 		}
-	  }, false);
+	}
 
-	document.addEventListener('wheel', function(e) {
-		if (e.deltaX < 0 && e.shiftKey && e.pageX >= window.innerWidth/2) {
+	fbf.injectControls = function() {
+		var controls_html = "<i class=\"icon icon-to-start\"></i><i class=\"icon icon-to-end\"></i>";
+		var fpsAndframeskip_html = "<b>FPS:&nbsp;" + fbf.FRAMES_PER_SECOND + "</b>\
+		&nbsp;&nbsp;<b>Frameskip:&nbsp;" + frameskip + "</b>";
+		var hotkeysButton_html = "<i class=\"icon icon-for-hotkeys-menu\"></i>";
+			
+		var newButtons = document.createElement('div');
+		newButtons.innerHTML = controls_html;
+		newButtons.style.float = 'left';
+		newButtons.style['margin-top'] = '2px';
+		
+		var hotkeysButton = document.createElement('div');
+		hotkeysButton.innerHTML = hotkeysButton_html;
+		hotkeysButton.style.float = 'right';
+		hotkeysButton.style['margin-top'] = '2px';
+		
+		var fpsAndframeskip = document.createElement('div');
+		fpsAndframeskip.innerHTML = fpsAndframeskip_html;
+		fpsAndframeskip.style.float = 'right';
+		fpsAndframeskip.style['margin-top'] = '2px';
+		
+		var child = document.getElementsByClassName('ytp-volume-hover-area')[0];
+		
+		control_bar.insertBefore(newButtons, child);
+		control_bar.insertBefore(hotkeysButton, child);
+		control_bar.insertBefore(fpsAndframeskip, child);
+		
+		fbf.updateFpsAndFS = function() {
+			fpsAndframeskip_html = "<b>FPS:&nbsp;" + fbf.FRAMES_PER_SECOND + "</b>\
+			&nbsp;&nbsp;<b>Frameskip:&nbsp;" + frameskip + "</b>";
+			fpsAndframeskip.innerHTML = fpsAndframeskip_html;
+		}
+		
+		var forward_button = document.getElementsByClassName("icon-to-end")[0];
+		forward_button.addEventListener('click', function() {
 			fbf.nextFrame(frameskip);
-		}
-		if (e.deltaX < 0 && e.shiftKey && e.pageX < window.innerWidth/2) {
+		});
+
+		var back_button = document.getElementsByClassName("icon-to-start")[0];
+		back_button.addEventListener('click', function() {
 			fbf.prevFrame(frameskip);
-		}
-	});
+		});
+		
+		hotkeysButton.addEventListener('click', function(){
+			newwindow=window.open("http://codepen.io/regularWand/full/eJdVep\
+			","name","height=510, width=467, top=125");
+			if (window.focus) {newwindow.focus()}
+		});
+	}
 
-	var search = document.getElementById("masthead-search-term");
-	search.addEventListener("focus", function(e) {
-		hotkeys = false;
-	});
-	search.addEventListener("blur", function(e) {
-		hotkeys = true;
-	});
+	if (document.getElementsByClassName("ytp-chrome-controls")[0]) {
+		fbf.injectControls();
+		
+		document.addEventListener("keydown", function(e) {
+		if (hotkeys) {
+				switch(e.which) {
+					case fbf.LEFT_SQUARE_BRACKET:
+						fbf.prevFrame(frameskip);
+						break;
+					case fbf.RIGHT_SQUARE_BRACKET:
+						fbf.nextFrame(frameskip);
+						break;
+					case fbf.COMMA:
+						fbf.multiplyFS("decrease");
+						break;
+					case fbf.PERIOD:
+						fbf.multiplyFS("increase");
+						break;
+					case fbf.P_KEY:
+						fbf.fbfPlayback();
+						break;
+					case fbf.O_KEY:
+						fbf.setFrameRate();
+						break;
+					case fbf.BACKSLASH_KEY:
+						fbf.hideControls();
+						break;
+				}
+			}
+		  }, false);
 
-	var comment = document.getElementById("watch-discussion");
-	comment.addEventListener("focus", function(e) {
-		hotkeys = false;
-	}, true);
-	comment.addEventListener("blur", function(e) {
-		hotkeys = true;
-	}, true);
-};
+		document.addEventListener('wheel', function(e) {
+			if (e.deltaX < 0 && e.shiftKey && e.pageX >= window.innerWidth/2) {
+				fbf.nextFrame(frameskip);
+			}
+			if (e.deltaX < 0 && e.shiftKey && e.pageX < window.innerWidth/2) {
+				fbf.prevFrame(frameskip);
+			}
+		});
+
+		var search = document.getElementById("masthead-search-term");
+		search.addEventListener("focus", function(e) {
+			hotkeys = false;
+		});
+		search.addEventListener("blur", function(e) {
+			hotkeys = true;
+		});
+
+		var comment = document.getElementById("watch-discussion");
+		comment.addEventListener("focus", function(e) {
+			hotkeys = false;
+		}, true);
+		comment.addEventListener("blur", function(e) {
+			hotkeys = true;
+		}, true);
+	};
+}
+
+window.onload(frameByFrame());
